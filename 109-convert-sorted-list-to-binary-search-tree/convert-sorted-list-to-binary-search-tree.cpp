cffa5ada4d1ba87034f69dc1a232830911fc1766
ajeetsingh12 <ajeetpundirnnt@gmail.com>
2026-07-28 23:45:10 +0530

    Time: 3 ms (96.70%) | Memory: 33.2 MB (39.51%) - LeetSync

---
+++ 109-convert-sorted-list-to-binary-search-tree/convert-sorted-list-to-binary-search-tree.cpp
@@ -0,0 +1,27 @@
+class Solution {
+private:
+    TreeNode* buildBST(ListNode* head, ListNode* tail) {
+        if (head == tail) {
+            return nullptr;
+        }
+
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while (fast != tail && fast->next != tail) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        TreeNode* root = new TreeNode(slow->val);
+        root->left = buildBST(head, slow);
+        root->right = buildBST(slow->next, tail);
+
+        return root;
+    }
+
+public:
+    TreeNode* sortedListToBST(ListNode* head) {
+        return buildBST(head, nullptr);
+    }
+};
