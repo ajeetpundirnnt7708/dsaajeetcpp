c51fed719f4a19b14626d090bd5309376f15b2dd
ajeetsingh12 <ajeetpundirnnt@gmail.com>
2026-09-05 23:06:17 +0530

    Time: 7 ms (77.75%) | Memory: 202.7 MB (76.32%) - LeetSync

---
+++ 4285-smallest-stable-index-ii/smallest-stable-index-ii.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int firstStableIndex(vector<int>& nums, int k) {
+        int n = nums.size();
+        
+        // Compute suffix minimums
+        vector<int> suffMin(n);
+        suffMin[n - 1] = nums[n - 1];
+        for (int i = n - 2; i >= 0; --i) {
+            suffMin[i] = min(nums[i], suffMin[i + 1]);
+        }
+        
+        // Track prefix maximum and find the first index satisfying the condition
+        int prefMax = nums[0];
+        for (int i = 0; i < n; ++i) {
+            prefMax = max(prefMax, nums[i]);
+            
+            if (prefMax - suffMin[i] <= k) {
+                return i;
+            }
+        }
+        
+        return -1;
+    }
+};
